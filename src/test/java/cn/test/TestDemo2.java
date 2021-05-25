e31b30900dc97a80fc232d819e6bea9c84b4f7bb
hxc <924150100@qq.ocm>
2021-05-25 19:39:26 +0800

    TestDemo2.java第一次提交

---
+++ src/test/java/cn/test/TestDemo2.java
@@ -0,0 +1,13 @@
+package cn.test;
+
+/**
+ * @Author: cong
+ * @Date: Created 2021/5/25 19:35
+ * @Description：
+ * @Version: 1.0
+ */
+public class TestDemo2 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
